05b2f5eddcc8c9da7707c881d4ef8c9952eee809
Jaewon Choi <jaewon.james.choi@gmail.com>
2017-07-18 18:53:54 +0900

    Make 3 diffed files
    1. A & B file
    2. A - B file
    3. B - A file

---
+++ README.md
@@ -0,0 +1,8 @@
+# Kernel config differ.
+
+* Usages
+```sh
+python3 kernelconfig-compare.py <config1> <config2>
+```
+
+Copyright at Jaewon Choi <jaewon.james.choi@gmail.com>
--- kernelconfig-compare.py
+++ kernelconfig-compare.py
@@ -3,14 +3,29 @@
 import sys
 
 def process(Aconfig_file, Bconfig_file):
-    pass
+    # TODO: Put the code to set file pointer to initial position.
+    A = { line.strip(' \n') for line in Aconfig_file if not line.startswith('#') }
+    B = { line.strip(' \n') for line in Bconfig_file if not line.startswith('#') }
+
+    with open('A_and_B.txt', 'w') as f:
+        for d in A & B:
+            if d != '':
+                f.write(d + '\n')
+    with open('A_minus_B.txt', 'w') as f:
+        for d in A - B:
+            if d != '':
+                f.write(d + '\n')
+    with open('B_minus_B.txt', 'w') as f:
+        for d in B - A:
+            if d != '':
+                f.write(d + '\n')
 
 def main():
     Aconfig_path = sys.argv[1]
     Bconfig_path = sys.argv[2]
 
-    with open(Aconfig_file, 'r') as Aconfig_file:
-        with open(Bconfig_file, 'r') as Bconfig_path:
+    with open(Aconfig_path, 'r') as Aconfig_file:
+        with open(Bconfig_path, 'r') as Bconfig_file:
             process(Aconfig_file, Bconfig_file)
 
 main()
