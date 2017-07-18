da16211c8bbbfde7999235b48a56fb78d4d81def
Jaewon Choi <jaewon.james.choi@gmail.com>
2017-07-18 19:58:01 +0900

    Add configuration alias

--- README.md
+++ README.md
@@ -3,12 +3,16 @@
 ## Usages
 ```sh
 python3 kernelconfig-compare.py <config1> <config2>
+
+# Or
+
+python3 kernelconfig-compare.py <config1> <config2> <config1-name> <config2-name>
 ```
 
 Then you could get three files,
-     * A_and_B.txt
-     * A_minus_B.txt
-     * B_minus_A.txt
+   * A_and_B.txt
+   * A_minus_B.txt
+   * B_minus_A.txt
 
 ## Note
 This script removes comments in linux kernel in default.
--- kernelconfig-compare.py
+++ kernelconfig-compare.py
@@ -2,20 +2,25 @@
 
 import sys
 
-def process(Aconfig_file, Bconfig_file):
+def process(Aconfig_file, Bconfig_file, Aheader, Bheader):
+    if Aheader == '':
+        Aheader = 'A'
+    if Bheader == '':
+        Bheader = 'B'
+
     # TODO: Put the code to set file pointer to initial position.
     A = { line.strip(' \n') for line in Aconfig_file if not line.startswith('#') }
     B = { line.strip(' \n') for line in Bconfig_file if not line.startswith('#') }
 
-    with open('A_and_B.txt', 'w') as f:
+    with open(Aheader + '_and_' + Bheader + '.txt', 'w') as f:
         for d in A & B:
             if d != '':
                 f.write(d + '\n')
-    with open('A_minus_B.txt', 'w') as f:
+    with open(Aheader + '_minus_' + Bheader + '.txt', 'w') as f:
         for d in A - B:
             if d != '':
                 f.write(d + '\n')
-    with open('B_minus_B.txt', 'w') as f:
+    with open(Bheader + '_minus_' + Aheader + '.txt', 'w') as f:
         for d in B - A:
             if d != '':
                 f.write(d + '\n')
@@ -24,8 +29,15 @@ def main():
     Aconfig_path = sys.argv[1]
     Bconfig_path = sys.argv[2]
 
+    Aheader = ''
+    Bheader = ''
+
+    if len(sys.argv) == 5:
+        Aheader = sys.argv[3]
+        Bheader = sys.argv[4]
+
     with open(Aconfig_path, 'r') as Aconfig_file:
         with open(Bconfig_path, 'r') as Bconfig_file:
-            process(Aconfig_file, Bconfig_file)
+            process(Aconfig_file, Bconfig_file, Aheader, Bheader)
 
 main()
